f98c73d8df75478efc8a72be53b9c6d3e32e2d8b
Atharavraj Dhumal <manasdhumal0@gmail.com>
2025-06-14 15:24:51 +0530

    online IDE done

---
+++ online-ide/src/app/layout.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from 'react';
+export const dynamic = 'force-dynamic';
+import LiveIDE from '@/LiveIDE/page';
+
+
+export default function Home() {
+  return (
+  <div>
+    <LiveIDE/>  
+  </div>
+  );
+}
